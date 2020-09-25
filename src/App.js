612e5e99495e00c05d88fbe69926f2367d7c70e9
Dubov_EA <Dubov_EA@surgutneftegas.ru>
2020-09-25 17:50:10 +0500

    lesson #5 1h:27min

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { Header } from "./components";
 import { Home, Cart } from "./pages";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/pizza.json")
+      .then((oResponse) => oResponse.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
